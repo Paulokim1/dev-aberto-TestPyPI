ed4bf3827133f3c6de1857c3de959f94127e0fb7
Paulo Kim <paulokim@Paulos-MacBook-Pro.local>
2022-09-27 11:05:13 -0300

    upload translation to pt_BR

--- pacote_exemplo/scripts/hello.py
+++ pacote_exemplo/scripts/hello.py
@@ -1,5 +1,14 @@
 from dev_aberto import hello
+from datetime import date 
+from dateutil import parser
+from babel.dates import format_datetime
+import gettext 
+
+gettext.install('cli', localedir='./locale') 
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    f_date = format_datetime(parser.parse(date), 'full')
+    last_commit_str = _('Last commit made at: ')
+    by_str = _('by')
+    print(last_commit_str + f_date + by_str + name)
